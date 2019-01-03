13291ef2f46f6d27df8ad90d4c82a4dd992f6782
Lucas Gomes <lucasgsm88@gmail.com>
2019-01-02 22:50:35 -0200

    Modelando a arquitetura do sistema

---
+++ backend/Persistencia/Contexts/UsuarioContext.cs
@@ -0,0 +1,10 @@
+﻿using Entidades.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistencia.Contexts
+{
+    public class UserContext: DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}

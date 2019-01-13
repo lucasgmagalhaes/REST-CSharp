4e3d38d7cf163b2aec2b2cbd4a2885bd07423fe9
Lucas Gomes <lucasgsm88@gmail.com>
2019-01-13 20:42:04 -0200

    configurando autenticação no front

---
+++ ClientApp/src/app/guards/jwt.interceptior.ts
@@ -0,0 +1,21 @@
+import { Authentication } from './authentication.model';
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // add authorization header with jwt token if available
+        let currentUser: Authentication = JSON.parse(localStorage.getItem('usuarioLogado'));
+        if (currentUser && currentUser.accessToken) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${currentUser.accessToken}`
+                }
+            });
+        }
+
+        return next.handle(request);
+    }
+}
